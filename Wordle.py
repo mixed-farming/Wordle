78171857da82639b96a43cce68a1806d28b19907
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-10-07 00:29:56 +0530

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,31 +1,39 @@
 import random
+import string
+
 from colorama import Fore, Back, Style
 
-def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
-def prGreen(skk): print("\033[92m{}\033[00m".format(skk),end="")
-def prYellow(skk): print("\033[93m{}\033[00m".format(skk),end="")
 
-def checkpoint(guess,a):
-    flag=1
-    for i in range(0,5,1):
-        if guess[i]==a[i]:
+def prRed(skk): print("\033[91m {}\033[00m".format(skk))
+
+
+def prGreen(skk): print("\033[92m{}\033[00m".format(skk), end="")
+
+
+def prYellow(skk): print("\033[93m{}\033[00m".format(skk), end="")
+
+
+def checkpoint(guess, a):
+    flag = 1
+    for i in range(0, 5, 1):
+        if guess[i] == a[i]:
             prGreen(guess[i])
             # print('The letter',guess[i],'is in the word and in the correct spot')
         else:
-            flag=0
-            for j in range(0,5,1):
-                if guess[i]==a[j]:
-                    flag=1;
+            flag = 0
+            for j in range(0, 5, 1):
+                if guess[i] == a[j]:
+                    flag = 1;
                     prYellow(guess[i])
                     # print('The letter', guess[i], 'is in the word but in the wrong spot')
                     break;
 
-            if flag==0:
-                print(guess[i],end="")
+            if flag == 0:
+                print(guess[i], end="")
                 # print('The letter', guess[i], 'is not in the word in any spot')
 
-title='Welcome to WORD-DARE'
-str=title.center(150)
+title = 'Welcome to WORD-DARE'
+str = title.center(150)
 print(Fore.BLACK + Back.GREEN + str)
 print(Style.RESET_ALL)
 with open("dictionary.txt", "r") as file:
@@ -34,25 +42,32 @@ with open("dictionary.txt", "r") as file:
 
     # Generating a random number for word position
     word_pos = random.randint(0, len(words) - 1)
-    word=words[word_pos]
+    word = words[word_pos]
     # print(word)
     # print("Word:", words[word_pos])
     # print("Position in the dictionary:", word_pos)
 
-guess1=input('\nEnter your first guess of the word : ')
+# alphabet_set=[]
+alphabet_set = list(string.ascii_lowercase)
+alphabet_set[0]= (Fore.GREEN + alphabet_set[0])
+print(alphabet_set[0])
+# print('Letter set :', Fore.YELLOW + alphabet_set[0],end=" ")
+# print(Fore.GREEN + alphabet_set[1])
+
+guess1 = input('\nEnter your first guess of the word : ')
 if guess1 not in data:
     while guess1 not in data:
-        guess1=input('Not in word list. Enter a valid word : ')
-if word==guess1:
+        guess1 = input('Not in word list. Enter a valid word : ')
+if word == guess1:
     prGreen(word)
     print('\nGENIUS!!!...That is correct\nYou won \U0001F929')
 else:
-    checkpoint(guess1,word)
+    checkpoint(guess1, word)
     guess2 = input('\n\nEnter your second guess of the word : ')
     if guess2 not in data:
         while guess2 not in data:
             guess2 = input('Not in word list. Enter a valid word : ')
-    if word==guess2:
+    if word == guess2:
         prGreen(word)
         print('\nMAGNIFICENT!...That is correct\nYou won \U0001F929')
     else:
@@ -92,5 +107,12 @@ else:
                         prGreen(word)
                         print('\nPhew!...That is correct\nYou won \U0001F929')
                     else:
-                        print('Answer : ',word)
+                        print('Answer : ', word)
                         print('Hard cheese \U00002639\t Better luck next time')
+
+'''
+TASKS TO BE DONE: 
+- word size == 5, !>5 && !<5
+- wordle engine to show possible answers
+- Generate alphabet set after every word entry
+'''
